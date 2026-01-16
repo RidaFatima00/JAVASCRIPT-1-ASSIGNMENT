2eaa207fdd4215b0e068494ae4ffd4832e86d5af
RIDA FATIMA <ridafatimaa.wahab7819@gmail.com>
2026-01-16 10:36:42 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,44 @@
+//============chapter 1===========
+
+//alert("hello")
+
+
+//============chapter 2===========
+
+//var Myname="Rida"
+//alert(Myname)
+//Myname="Rida fatima"
+//alert(Myname)
+
+
+//============chapter 3===========
+
+//var weight=100;
+//alert(weight)
+//weight=100+10
+//alert(weight)
+
+
+//============chapter 4===========
+
+//var myFathername="A.wahab"
+//alert(myFathername)
+
+
+//============chapter 5===========
+
+
+//var popularNumber=2+2;
+//alert(2+2);
+
+//var popularNumber=50-25;
+//alert(50-25);
+
+//var popularNumber=10*10;
+//alert(10*10);
+
+//var popularNumber=20/5;
+//alert(20/5);
+
+//var whatsLeftover=9 % 3;
+//alert(9 % 3);
